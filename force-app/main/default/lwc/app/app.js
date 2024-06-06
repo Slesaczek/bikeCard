33d76b766bec67daba0e5c5c068e7ba569df92cb
César Slesaczek <cesarslesac@hotmail.com>
2024-06-06 19:08:54 -0300

    Início do projeto Noções básicas dos Componentes Web do Lightning

---
+++ force-app/main/default/lwc/app/app.js
@@ -0,0 +1,8 @@
+// app.js
+import { LightningElement } from 'lwc';
+export default class App extends LightningElement {
+  bike = {
+    name: 'Electra X4',
+    picture: 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg'
+  };
+}
---
+++ force-app/main/default/lwc/bike/bike.js
@@ -0,0 +1,5 @@
+// bike.js 
+import { LightningElement, api } from 'lwc';
+export default class Bike extends LightningElement {
+  @api bike;
+}
